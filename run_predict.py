398978ae23fdd1e96363bc5f9a0263202a9b58ba
ailcxf82 <lu.feng.2006@gmail.com>
2025-12-11 21:44:45 +0800

    V0.1.0

--- run_predict.py
+++ run_predict.py
@@ -24,7 +24,7 @@ def parse_args():
     parser.add_argument(
         "--start",
         type=str,
-        default=os.environ.get("RUN_PRED_START", "2021-10-01"),
+        default=os.environ.get("RUN_PRED_START", "2023-10-01"),
         help="预测起始日期，默认 2018-01-01，可通过环境变量 RUN_PRED_START 覆盖",
     )
     parser.add_argument(
